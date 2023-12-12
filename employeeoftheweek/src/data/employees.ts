64571046629f129261570f9bf4d31fe119e18814
Andreas Wolf <andreas.wolf@diva-e.com>
2023-12-12 14:54:21 +0100

    new employees

--- employeeoftheweek/src/data/employees.ts
+++ employeeoftheweek/src/data/employees.ts
@@ -1,172 +1,307 @@
 import { Employee } from "../components/employee-card/employee-card";
 
 export const employees: Employee[] = [
-  {name: "Julia", image: new URL(
-    '../assets/images/julia.jpg?as=webp&width=500',
-    import.meta.url
-  ),
-  confetti: ["🐕", "🦴"],
-  gender: 'female'
-},
+  {
+    name: "Julia",
+    image: new URL(
+      "../assets/images/julia.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    confetti: ["🐕", "🦴"],
+    gender: "female",
+  },
   {
     name: "Andi",
-    image: new URL('../assets/images/andi.jpg?as=webp&width=500', import.meta.url),
+    image: new URL(
+      "../assets/images/andi.jpg?as=webp&width=500",
+      import.meta.url
+    ),
     confetti: ["🚀", "⭐", "🪐"],
-    gender: 'male'
+    gender: "male",
   },
   {
     name: "Alex",
-    image: new URL('../assets/images/alex.jpg?as=webp&width=500', import.meta.url),
-    gender: 'male'
+    image: new URL(
+      "../assets/images/alex.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "male",
   },
   {
     name: "Jan",
-    image: new URL('../assets/images/jan.jpg?as=webp&width=500', import.meta.url),
+    image: new URL(
+      "../assets/images/jan.jpg?as=webp&width=500",
+      import.meta.url
+    ),
     confetti: ["⚔️", "🛡️"],
-    gender: 'male'
+    gender: "male",
   },
   {
     name: "Johanna",
-    image: new URL('../assets/images/johanna.jpg?as=webp&width=500', import.meta.url),
-    gender: 'female'
+    image: new URL(
+      "../assets/images/johanna.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "female",
   },
   {
     name: "Kolja",
-    image: new URL('../assets/images/kolja.jpg?as=webp&width=500', import.meta.url),
-    gender: 'male'
+    image: new URL(
+      "../assets/images/kolja.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "male",
   },
   {
     name: "Lena",
-    image: new URL('../assets/images/lena.jpg?as=webp&width=500', import.meta.url),
-    gender: 'female'
+    image: new URL(
+      "../assets/images/lena.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "female",
   },
   {
     name: "Michael",
-    image: new URL('../assets/images/michael.jpg?as=webp&width=500', import.meta.url),
-    gender: 'male'
+    image: new URL(
+      "../assets/images/michael.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "male",
   },
   {
     name: "Norma",
-    image: new URL('../assets/images/norma.jpg?as=webp&width=500', import.meta.url),
+    image: new URL(
+      "../assets/images/norma.jpg?as=webp&width=500",
+      import.meta.url
+    ),
     confetti: ["🐴", "🐎"],
-    gender: 'female'
+    gender: "female",
   },
   {
     name: "Oskar",
-    image: new URL('../assets/images/oskar.jpg?as=webp&width=500', import.meta.url),
-    gender: 'male'
+    image: new URL(
+      "../assets/images/oskar.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "male",
   },
   {
     name: "Pabu",
-    image: new URL('../assets/images/pabu.jpg?as=webp&width=500', import.meta.url),
+    image: new URL(
+      "../assets/images/pabu.jpg?as=webp&width=500",
+      import.meta.url
+    ),
     confetti: ["🐕", "🦴"],
-    gender: 'male'
+    gender: "male",
   },
   {
     name: "Verena",
-    image: new URL('../assets/images/verena.jpg?as=webp&width=500', import.meta.url),
-    gender: 'female'
+    image: new URL(
+      "../assets/images/verena.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "female",
   },
   {
     name: "Holger",
-    image: new URL('../assets/images/holger.jpg?as=webp&width=500', import.meta.url),
-    gender: 'male'
+    image: new URL(
+      "../assets/images/holger.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "male",
   },
   {
     name: "Rene",
-    image: new URL('../assets/images/rene.jpg?as=webp&width=500', import.meta.url),
-    gender: 'male'
+    image: new URL(
+      "../assets/images/rene.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "male",
   },
   {
     name: "Tanja",
-    image: new URL('../assets/images/tanja.jpg?as=webp&width=500', import.meta.url),
-    gender: 'female'
+    image: new URL(
+      "../assets/images/tanja.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "female",
   },
   {
     name: "Lisa",
-    image: new URL('../assets/images/lisa.jpg?as=webp&width=500', import.meta.url),
-    gender: 'female'
+    image: new URL(
+      "../assets/images/lisa.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "female",
   },
   {
     name: "Lukas",
-    image: new URL('../assets/images/lukas.jpeg?as=webp&width=500', import.meta.url),
-    gender: 'male'
+    image: new URL(
+      "../assets/images/lukas.jpeg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "male",
   },
   {
     name: "Kristina",
-    image: new URL('../assets/images/kristina.jpg?as=webp&width=500', import.meta.url),
-    gender: 'female'
+    image: new URL(
+      "../assets/images/kristina.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "female",
   },
   {
     name: "Annika",
-    image: new URL('../assets/images/annika.jpeg?as=webp&width=500', import.meta.url),
-    gender: 'female'
+    image: new URL(
+      "../assets/images/annika.jpeg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "female",
   },
   {
     name: "Lulu",
-    image: new URL('../assets/images/lulu.jpg?as=webp&width=500', import.meta.url),
-    gender: 'female',
-    confetti: ["🐕", "🦴"]
+    image: new URL(
+      "../assets/images/lulu.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "female",
+    confetti: ["🐕", "🦴"],
   },
   {
     name: "Jan S.",
-    image: new URL('../assets/images/jan_s.jpg?as=webp&width=500', import.meta.url),
-    gender: 'male'
+    image: new URL(
+      "../assets/images/jan_s.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "male",
   },
   {
     name: "Fionn",
-    image: new URL('../assets/images/fionn.jpg?as=webp&width=500', import.meta.url),
-    gender: 'male'
+    image: new URL(
+      "../assets/images/fionn.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "male",
   },
   {
     name: "Sonja",
-    image: new URL('../assets/images/sonja.jpg?as=webp&width=500', import.meta.url),
-    gender: 'female'
+    image: new URL(
+      "../assets/images/sonja.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "female",
   },
   {
     name: "Stella",
-    image: new URL('../assets/images/stella.jpg?as=webp&width=500', import.meta.url),
-    gender: 'female'
+    image: new URL(
+      "../assets/images/stella.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "female",
   },
   {
     name: "Iris",
-    image: new URL('../assets/images/iris.jpg?as=webp&width=500', import.meta.url),
-    gender: 'female'
+    image: new URL(
+      "../assets/images/iris.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "female",
   },
   {
     name: "Rasti",
-    image: new URL('../assets/images/rasti.jpg?as=webp&width=500', import.meta.url),
-    gender: 'female',
-    confetti: ["🐕", "🦴"]
+    image: new URL(
+      "../assets/images/rasti.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "female",
+    confetti: ["🐕", "🦴"],
   },
   {
     name: "Ralph",
-    image: new URL('../assets/images/ralph.jpg?as=webp&width=500', import.meta.url),
-    gender: 'male'
+    image: new URL(
+      "../assets/images/ralph.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "male",
   },
   {
     name: "Noel",
-    image: new URL('../assets/images/noel.jpg?as=webp&width=500', import.meta.url),
-    gender: 'male'
+    image: new URL(
+      "../assets/images/noel.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "male",
   },
   {
     name: "Kris",
-    image: new URL('../assets/images/kris.jpg?as=webp&width=500', import.meta.url),
-    gender: 'female'
+    image: new URL(
+      "../assets/images/kris.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "female",
   },
   {
     name: "Christian",
-    image: new URL('../assets/images/christian.jpg?as=webp&width=500', import.meta.url),
-    gender: 'male'
+    image: new URL(
+      "../assets/images/christian.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "male",
   },
   {
     name: "Christoph",
-    image: new URL('../assets/images/christoph.jpg?as=webp&width=500', import.meta.url),
-    gender: 'male'
+    image: new URL(
+      "../assets/images/christoph.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "male",
   },
   {
     name: "Jan",
-    image: new URL('../assets/images/jan_sc.jpg?as=webp&width=500', import.meta.url),
-    gender: 'male'
+    image: new URL(
+      "../assets/images/jan_sc.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "male",
+  },
+  {
+    name: "Maria",
+    image: new URL(
+      "../assets/images/maria.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "female",
+  },
+  {
+    name: "Destina",
+    image: new URL(
+      "../assets/images/destina.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "female",
+  },
+  {
+    name: "Sergii",
+    image: new URL(
+      "../assets/images/sergii.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "male",
+  },
+  {
+    name: "Maraike",
+    image: new URL(
+      "../assets/images/maraike.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "female",
+  },
+  {
+    name: "Lara",
+    image: new URL(
+      "../assets/images/lara.jpg?as=webp&width=500",
+      import.meta.url
+    ),
+    gender: "female",
   },
 ];
